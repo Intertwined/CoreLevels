5958e43c1398a90ee8b9fa1ee2ff693fe49a8ff9
NuclearW <incongruency@gmail.com>
2013-01-20 21:52:34 -0500

    Start manager

--- src/main/java/com/nuclearw/intertwined/corelevels/SimpleCoreLevelsManager.java
+++ src/main/java/com/nuclearw/intertwined/corelevels/SimpleCoreLevelsManager.java
@@ -3,5 +3,9 @@ package com.nuclearw.intertwined.corelevels;
 import com.nuclearw.intertwined.corelevels.api.CoreLevelsManager;
 
 public class SimpleCoreLevelsManager implements CoreLevelsManager {
+	private CoreLevels plugin;
 
+	public SimpleCoreLevelsManager(CoreLevels plugin) {
+		this.plugin = plugin;
+	}
 }
